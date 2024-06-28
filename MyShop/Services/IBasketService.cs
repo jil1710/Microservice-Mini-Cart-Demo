982d3d02a1da3a1be4bd83e3065577f180276717
jil1710 <jil.p@simformsolutions.com>
2024-06-28 18:18:30 +0530

    add: cart demo using microservies

---
+++ MyShop/Services/IBasketService.cs
@@ -0,0 +1,12 @@
+﻿using MyShop.Models;
+using System.Threading.Tasks;
+
+namespace MyShop.Services
+{
+    public interface IBasketService
+    {
+        Task<BasketModel> GetBasket(string userName);
+        Task<BasketModel> UpdateBasket(BasketModel model);
+        Task CheckoutBasket(BasketCheckoutModel model);
+    }
+}
---
+++ MyShop/Services/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using MyShop.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyShop.Services
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+    }
+}
